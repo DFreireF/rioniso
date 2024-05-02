4d5aa4257204f25f354e6d1e639ed903f866910c
DFreireF <D.FreireFernandez@gsi.de>
2024-05-02 12:43:39 +0200

    improving Plotters structure | jue 02 may 2024 12:43:39 CEST

--- rioniso/plotters.py
+++ rioniso/plotters.py
@@ -1,16 +1,15 @@
 import matplotlib.pyplot as plt
-from adjustText import adjust_text
 import textalloc as ta
 import numpy as np
 
 class Plotters(object):
     def __init__(self):
-        self.mean_freq = np.array([])
-        self.sigma_freq = np.array([])
+        self.mean_freq      = np.array([])
+        self.sigma_freq     = np.array([])
         self.sigma_freq_err = np.array([])
-        self.names = np.array([])
-        self.xfit = np.array([])
-        self.yfit = np.array([])
+        self.names          = np.array([])
+        self.xfit           = np.array([])
+        self.yfit           = np.array([])
         self.fit_parameters = np.array([])
 
     @classmethod
@@ -19,18 +18,18 @@ class Plotters(object):
         obj.mean_freq = np.asarray(iso_data[:, 1], dtype=float)*1e-6
         obj.sigma_freq = np.asarray(iso_data[:, 3], dtype=float)
         obj.sigma_freq_err = np.asarray(iso_data[:, 4], dtype=float)
-        obj.names = iso_data[:,0]
+        obj.names = iso_data[:, 0]
         obj.xfit = xfit
         obj.yfit = yfit
         obj.fit_parameters = fit_parameters
         return obj
 
-    def _plot(obj):
-        obj._set_fig()
-        obj._plot_fit(obj.xfit*1e-6, obj.yfit, *obj.fit_parameters)
-        obj.axs.legend(shadow=True,fancybox=True, fontsize = 13)
-        obj._plot_data()
-        obj.handle_labels(obj.names)
+    def _plot(self):
+        self._set_fig()
+        self._plot_fit(self.xfit*1e-6, self.yfit, *self.fit_parameters)
+        self.axs.legend(shadow=True,fancybox=True, fontsize = 13)
+        self._plot_data()
+        self.handle_labels(self.names)
         plt.show()
 
     def _set_fig(self):
@@ -51,16 +50,3 @@ class Plotters(object):
     def handle_labels(self, names_latex):
         ta.allocate_text(self.fig, self.axs, self.mean_freq, self.sigma_freq, names_latex, x_scatter=self.mean_freq, y_scatter=self.sigma_freq,
                         textsize=15, nbr_candidates=5000, seed=42, min_distance=0.05, max_distance=0.4)
-
-
-        
-
-
-#    
-#    
-#    axs.errorbar(T_y, y_sigmaT_vals_Y, yerr=np.sqrt(y_sigmaT_errs_Y**2+sigma_sys**2), fmt='o', #label="Calibrant ion",
-#                    markersize='13', ecolor='black',capsize=4, elinewidth=3, color='blue')
-#    
-#    axs.plot(x_fit, iso_curve(x_fit, *fit_params), 'r-', label=label_axs2,  linewidth=5)
-
-#    return axs
