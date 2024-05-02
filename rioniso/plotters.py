ab92dbb495843a95592897a3ca43e449b23d07c4
DFreireF <D.FreireFernandez@gsi.de>
2024-05-02 12:52:10 +0200

    improving Plotters structure | jue 02 may 2024 12:52:10 CEST

--- rioniso/plotters.py
+++ rioniso/plotters.py
@@ -19,34 +19,34 @@ class Plotters(object):
         obj.sigma_freq = np.asarray(iso_data[:, 3], dtype=float)
         obj.sigma_freq_err = np.asarray(iso_data[:, 4], dtype=float)
         obj.names = iso_data[:, 0]
-        obj.xfit = xfit
+        obj.xfit = xfit*1e-6
         obj.yfit = yfit
         obj.fit_parameters = fit_parameters
         return obj
 
-    def _plot(self):
+    def _plot(self, fs = 13):
         self._set_fig()
-        self._plot_fit(self.xfit*1e-6, self.yfit, *self.fit_parameters)
-        self.axs.legend(shadow=True,fancybox=True, fontsize = 13)
+        self._plot_fit(*self.fit_parameters)
+        self.axs.legend(shadow=True,fancybox=True, fontsize = fs)
         self._plot_data()
-        self.handle_labels(self.names)
+        self.handle_labels()
         plt.show()
 
-    def _set_fig(self):
-        fontsize = 18
-        self.fig, self.axs = plt.subplots(1, 1, figsize=(15, 8))
+    def _set_fig(self, width=15, height=8, dpi=300, fs = 18):
+        fontsize = fs
+        self.fig, self.axs = plt.subplots(1, 1, figsize=(width, height), dpi = dpi)
         self.axs.set_xlabel(r"Revolution frequency (MHz)", fontsize = fontsize)
         self.axs.set_ylabel(r"$\sigma_{f}$ (Hz)", fontsize = fontsize)
         self.axs.grid(True, linestyle=':', color='grey',alpha = 0.7)
         self.axs.tick_params(axis='both', which='major', labelsize=fontsize)
 
-    def _plot_fit(self, xfit, yfit, gammat, dpop, sigma_sys):
+    def _plot_fit(self, gammat, dpop, sigma_sys):
         label = f'$\sqrt{{\left(1 - \left(\\frac{{f \cdot 108.36}}{{c}}\\right)^2 - \\frac{{1}}{{{gammat:.4f}^2}}\\right)^2 \cdot \\left(\\frac{{{np.abs(dpop)*1e2:.4f}}}{{100}} \cdot f\\right)^2 + {abs(sigma_sys):.4f}^2}}$'
-        self.axs.plot(xfit, yfit, '-', color='blue',  linewidth=5, label = label)
+        self.axs.plot(self.xfit, self.yfit, '-', color='blue',  linewidth=5, label = label)
     
     def _plot_data(self):
         self.axs.errorbar(self.mean_freq, self.sigma_freq, yerr=self.sigma_freq_err, color = 'black', fmt = 'o', picker = 5)
 
-    def handle_labels(self, names_latex):
-        ta.allocate_text(self.fig, self.axs, self.mean_freq, self.sigma_freq, names_latex, x_scatter=self.mean_freq, y_scatter=self.sigma_freq,
+    def handle_labels(self):
+        ta.allocate_text(self.fig, self.axs, self.mean_freq, self.sigma_freq, self.names, x_scatter=self.mean_freq, y_scatter=self.sigma_freq,
                         textsize=15, nbr_candidates=5000, seed=42, min_distance=0.05, max_distance=0.4)
