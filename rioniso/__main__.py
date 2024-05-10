191cecbc0773c2d778a9b239733d32efcd165ac3
DFreireF <D.FreireFernandez@gsi.de>
2024-05-10 14:32:03 +0200

    improving structure | vie 10 may 2024 14:32:03 CEST

--- rioniso/__main__.py
+++ rioniso/__main__.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument('identdata', type=str, help='Input file from identification (ODS format).')
 
     args = parser.parse_args()
-    
+
     # Importing data
     mydata = ImportData(args.identdata, args.expdata)
     experimental_data = mydata.experimental_data
@@ -19,8 +19,6 @@ def main():
     # Calculating iso curve experimental data
     iso_data = calculate_iso_inputs(imported_data.simulated_data, 
                                 imported_data.experimental_data, xspan=6e3)
-    
-
 
 if __name__ == "__main__":
     main()
