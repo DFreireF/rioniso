ed65c5f4d67ae3b1d3f1db43cd5cd8f9a018e97f
DFreireF <D.FreireFernandez@gsi.de>
2024-05-10 14:30:49 +0200

    improving structure | vie 10 may 2024 14:30:49 CEST

--- rioniso/__main__.py
+++ rioniso/__main__.py
@@ -9,6 +9,8 @@ def main():
     parser.add_argument('expdata', type=str, help='Input file of with the experimental data (npz format).')
     parser.add_argument('identdata', type=str, help='Input file from identification (ODS format).')
 
+    args = parser.parse_args()
+    
     # Importing data
     mydata = ImportData(args.identdata, args.expdata)
     experimental_data = mydata.experimental_data
